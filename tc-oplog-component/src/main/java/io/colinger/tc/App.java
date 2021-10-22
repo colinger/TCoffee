fdb0288ab7de4deb44480a8281259a604ce9080e
xinying.ge <xinying.ge@aispeech.com>
2021-10-22 20:50:23 +0800

    init

---
+++ tc-oplog-component/src/main/java/io/colinger/tc/App.java
@@ -0,0 +1,13 @@
+package io.colinger.tc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
